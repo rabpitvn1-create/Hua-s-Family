eea50eaab19473435d062d403dd611e63e2efdd6
rabpitvn1-create <rabpitvn1@gmail.com>
2026-07-31 07:04:22 +0700

    Nạp avatar sau khi khởi tạo game sandbox

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { initGame } from "./game.js";
 import { initAiGameMaster } from "./ai-game-master.js";
+import { initDialogueAvatars } from "./dialogue-avatars.js";
 
 const bridge = initGame();
+initDialogueAvatars();
 if (bridge) initAiGameMaster(bridge);
